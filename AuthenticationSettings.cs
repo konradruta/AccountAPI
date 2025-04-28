376b83965f0a0d3f3c58a060ca2c5efba107975a
konradruta <konrad.rutaa@gmail.com>
2025-04-28 13:45:17 +0200

    Add files via upload

---
+++ AuthenticationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace AccountAPI
+{
+    public class AuthenticationSettings
+    {
+        public string JwtKey {  get; set; }
+        public string JwtIssuer { get; set; }
+        public int JwtExpiresDays { get; set; }
+    }
+}
---
+++ IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace AccountAPI
+{
+    public interface IEmailSender
+    {
+        void SendEmail(string to, string subject, string body);
+    }
+}
