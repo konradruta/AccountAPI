76b5f2843a4f0c1b72c6782d299ec4568b9a7222
Konrad Ruta <konrad.rutaa@gmail.com>
2026-02-18 13:59:44 +0100

    Dodanie testów integracyjnych API (rejestracja, logowanie, endpointy autoryzowane)

---
+++ AccountAPI.IntegrationTests/AuthIntegrationTests.cs
@@ -0,0 +1,117 @@
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace AccountAPI.IntegrationTests
+{
+    public class AuthIntegrationTests : IClassFixture<AccountApiFactory>
+    {
+        private readonly HttpClient _client;
+
+        public AuthIntegrationTests(AccountApiFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Register_Should_Return_OK()
+        {
+            var request = new
+            {
+                email = "test1@test.com",
+                name = "Testowy",
+                password = "Password123!",
+                confirmPassword = "Password123!"
+            };
+
+            var response = await _client.PostAsJsonAsync("/api/user/register", request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Login_Should_Return_Jwt()
+        {
+            // REGISTER
+            var registerRequest = new
+            {
+                email = "test2@test.pl",
+                name = "Testowy2",
+                password = "Password123!",
+                confirmPassword = "Password123!"
+            };
+
+            await _client.PostAsJsonAsync("/api/user/register", registerRequest);
+
+            // LOGIN
+            var request = new
+            {
+                email = "test2@test.pl",
+                password = "Password123!"
+            };
+
+            var response = await _client.PostAsJsonAsync("/api/user/login", request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var result = await response.Content.ReadFromJsonAsync<LoginResponse>();
+
+            result.Should().NotBeNull();
+            result!.AccessToken.Should().NotBeNullOrEmpty();
+            result!.RefreshToken.Should().NotBeNullOrEmpty();
+        }
+
+
+        public class LoginResponse
+        {
+            public string? AccessToken { get; set; }
+            public string? RefreshToken { get; set; }
+        }
+
+        [Fact]
+        public async Task GetMe_With_Token_Should_Return_200()
+        {
+            // REGISTER
+            var registerRequest = new
+            {
+                email = "test@test.com",
+                name = "Test User",
+                password = "Password123!",
+                confirmPassword = "Password123!",
+            };
+
+            await _client.PostAsJsonAsync("/api/user/register", registerRequest);
+
+            // LOGIN
+            var loginRequest = new
+            {
+                email = "test@test.com",
+                password = "Password123!"
+            };
+
+            var loginResponse = await _client.PostAsJsonAsync("/api/user/login", loginRequest);
+            loginResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var loginResult = await loginResponse.Content.ReadFromJsonAsync<LoginResponse>();
+
+            // ADD JWT
+            _client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", loginResult!.AccessToken);
+
+            // CALL ME
+            var response = await _client.GetAsync("/api/user/me");
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task Protected_Endpoint_Without_Token_Should_Return_401()
+        {
+            var response = await _client.GetAsync("/api/user/me");
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+    }
+}
---
+++ AccountAPI.IntegrationTests/WebApplicationFactoryContentRootAttribute.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+[assembly: WebApplicationFactoryContentRoot(
+    "AccountAPI",
+    @"..\..\..\..\AccountAPI",
+    "AccountAPI.csproj",
+    "")]
