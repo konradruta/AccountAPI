bcd0aff8f0d3d7a33d2bac6e313fbba7b439f4dc
Konrad Ruta <konrad.rutaa@gmail.com>
2026-02-22 23:48:58 +0100

    test: dodanie testów integracyjnych rate limiting dla logowania oraz refresh

---
+++ AccountAPI.IntegrationTests/AccountApiFactoryWithRateLimit.cs
@@ -0,0 +1,39 @@
+﻿using AccountAPI.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountAPI.IntegrationTests
+{
+    public class AccountApiFactoryWithRateLimit : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Development");
+
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<AccountDbContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<AccountDbContext>(options =>
+                    options.UseInMemoryDatabase("AccountTestDb"));
+
+                var sp = services.BuildServiceProvider();
+                using var scope = sp.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<AccountDbContext>();
+                db.Database.EnsureCreated();
+            });
+        }
+    }
+}
---
+++ AccountAPI.IntegrationTests/RateLimitIntegrationTests.cs
@@ -0,0 +1,92 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountAPI.IntegrationTests
+{
+    public class RateLimitIntegrationTests : IClassFixture<AccountApiFactoryWithRateLimit>
+    {
+        private readonly HttpClient _client;
+
+        public RateLimitIntegrationTests(
+            AccountApiFactoryWithRateLimit factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Login_Should_Return_429_When_Limit_Exceeded()
+        {
+            var email = $"{Guid.NewGuid()}@test.com";
+
+            var register = new
+            {
+                email,
+                name = "Test",
+                password = "Password123!",
+                confirmPassword = "Password123!"
+            };
+
+            await _client.PostAsJsonAsync("/api/user/register", register);
+
+            var login = new
+            {
+                email,
+                password = "Password123!"
+            };
+
+            HttpResponseMessage? response = null;
+
+            for (int i = 0; i < 11; i++)
+            {
+                response = await _client.PostAsJsonAsync(
+                    "/api/user/login",
+                    login);
+            }
+
+            response!.StatusCode
+                .Should()
+                .Be(HttpStatusCode.TooManyRequests);
+        }
+
+        [Fact]
+        public async Task Refresh_Should_Return_429_When_Limit_Exceeded()
+        {
+            var email = $"{Guid.NewGuid()}@test.com";
+
+            var register = new
+            {
+                email,
+                name = "Test123",
+                password = "Password123!",
+                confirmPassword = "Password123!"
+            };
+
+            await _client.PostAsJsonAsync("/api/user/register", register);
+
+            var login = new
+            {
+                email,
+                password = "Password123!"
+            };
+
+            HttpResponseMessage? response = null;
+
+            for (int i = 0; i < 16; i++)
+            {
+                response = await _client.PostAsJsonAsync(
+                    "/api/user/refresh",
+                    login);
+            }
+
+            response!.StatusCode
+                .Should()
+                .Be(HttpStatusCode.TooManyRequests);
+        }
+    }
+}
